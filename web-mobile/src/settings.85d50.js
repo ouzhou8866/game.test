23229bbbf4aab671484f4030431c836f87ceeda9
ouzhou8866 <245508489@qq.com>
2018-02-13 14:03:08 +0800

    测试游戏

---
+++ web-mobile/src/settings.85d50.js
@@ -0,0 +1,80 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "dcGU8ayKVGyqr58Dtwl0r1": [
+                "audio/jump.mp3",
+                0
+            ],
+            "550RC5urZNnZQWA+RpBPo1": [
+                "audio/score.mp3",
+                0
+            ],
+            "f3d5R3fb9HUZAvmFlzcgFS": [
+                "mikado_outline_shadow.png",
+                1
+            ],
+            "c6tf0Tan9Bh4cWEYnl0Twn": [
+                "textures/PurpleMonster.png",
+                1
+            ],
+            "0a8z+LRRJFL4/64Bdc7nZ8": [
+                "textures/background.jpg",
+                1
+            ],
+            "3e2Ug6031GmYdik5kQw+En": [
+                "textures/ground.png",
+                1
+            ],
+            "80x7HnHfdPx5TlxTBzhCDt": [
+                "textures/star.png",
+                1
+            ]
+        }
+    },
+    assetTypes: [
+        "cc.AudioClip",
+        "cc.Texture2D"
+    ],
+    launchScene: "db://assets/game.fire",
+    scenes: [
+        {
+            url: "db://assets/game.fire",
+            uuid: 0
+        }
+    ],
+    packedAssets: {
+        "0bdb42cb2": [
+            "11uCRMttlFd7N+YnuVInhI",
+            0,
+            "42ZifUFO9HG6/unBZYJRqs",
+            "49LBk7l6RC87epam5M7MDp",
+            "6956FaQUNCrrrEHex8W8lU",
+            "8fi6VPcPZMoYbZ3dvYXnad",
+            "97fP6bIcdJjrBgVBgAURqL",
+            "a70y/BSilJHqXiCJIGPTXQ"
+        ]
+    },
+    orientation: "",
+    uuids: [
+        "3dkig3O4RINIphsbA762/l"
+    ],
+    "md5AssetsMap": {
+        "0b/0bdb42cb2.json": "558eb",
+        "assets/audio/jump.mp3": "74fd1",
+        "assets/audio/score.mp3": "fe7c9",
+        "assets/mikado_outline_shadow.png": "c1880",
+        "assets/textures/background.jpg": "0c687",
+        "assets/textures/ground.png": "5fdc8",
+        "assets/textures/PurpleMonster.png": "4b0bd",
+        "assets/textures/star.png": "c0cb3"
+    }
+};
